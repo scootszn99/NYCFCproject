05d927a3f7a77591730834a83a6165d6752fd657
Nayan <nayanptl99@gmail.com>
2021-03-06 16:06:30 -0500

    Initial commit

---
+++ datacleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from pandas import DataFrame
+
+# reading in the raw data
+df = pd.read_csv("goalsadded.csv")
+df2 = pd.read_csv("salaries.csv")
+
+# dropping phantom unnamed columns
+df = df.drop(df.columns[[0]], axis=1)
+df2 = df2.drop(df2.columns[[0]], axis=1)
+
+# filtering salary data for only September dates to have one entry per player each season
+df2['Date'] = pd.to_datetime(df2['Date'], errors='coerce')
+df2 = df2[df2['Date'].dt.month == 9]
+
+# merging goals added and salary data
+df = pd.merge(df, df2, how="left", on=["Player", "Season"])
+
+# dropping redundant columns
+df = df.drop(['Team_y', 'Position_y', 'Date'], axis=1)
+
+# exporting ready to use data
+df.to_csv('exportedmetrics.csv', index=False)
