2acc4a7f5df4772808e3d0a6ae05cd2e45479e4e
Nayan <nayanptl99@gmail.com>
2021-03-06 17:07:32 -0500

    fixed players with multiple teams per season

--- datacleaning.py
+++ datacleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
+import numpy as np
 
 # reading in the raw data
 df = pd.read_csv("goalsadded.csv")
@@ -20,5 +21,9 @@ df = pd.merge(df, df2, how="left", on=["Player", "Season"])
 df = df.drop(['Team_y', 'Position_y', 'Date'], axis=1)
 df = df.rename(columns={"Team_x": "Team", "Position_x": "Position"})
 
+# adjusting for players that switch to different team mid-season for logo purposes
+df[['Team', 'Team 2']] = df['Team'].str.split(', ', 1, expand=True)
+df = df.replace(r'^\s*$', np.nan, regex=True)
+
 # exporting ready to use data
-df.to_csv('exportedmetrics.csv', index=False)
+df.to_csv('exportedmetrics.csv', index=False, encoding='utf-8-sig')
